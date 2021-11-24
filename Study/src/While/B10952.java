5de68c06b25fbb6260334998992062fbbc1bb488
pnam9 <parkmac93@gmail.com>
2021-11-24 09:11:29 +0900

    While 합구하기

---
+++ Study/src/While/B10952.java
@@ -0,0 +1,23 @@
+package While;
+//	A+B - 5 합구하기
+
+import java.util.Scanner;
+
+public class B10952 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if(a==0 && b==0) {
+				sc.close();
+				break;
+			}
+			System.out.println(a+b);
+		}
+	}
+}
