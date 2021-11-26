ebfb664308fa873b2e0b54c4e20f0da8c01962a9
pnam9 <parkmac93@gmail.com>
2021-11-26 09:07:07 +0900

    Create B11654.java

---
+++ Study/src/Day01/B11654.java
@@ -0,0 +1,18 @@
+package Day01;
+
+import java.util.Scanner;
+
+//ASCII 코드 출력하기
+
+
+public class B11654 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.next().charAt(0);
+		
+		System.out.println(a);
+	}
+}
