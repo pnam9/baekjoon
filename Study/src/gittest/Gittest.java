c571070fc1e3175394ce53e50eb6c0addaa48831
pnam9 <parkmac93@gmail.com>
2021-11-30 13:41:35 +0900

    eclipse test

--- Study/src/gittest/Gittest.java
+++ Study/src/gittest/Gittest.java
@@ -1,10 +1,18 @@
 package gittest;
 
+import java.util.Scanner;
+
 public class Gittest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.next();
+		
+		sc.close();
+		
+		System.out.println(a);
 	}
 
 }
